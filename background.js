86d3f19905ddb46b3faf3f00d18fa212bfb249e6
BiliSub <skelitalynn@users.noreply.github.com>
2026-06-22 17:19:30 +0800

    Revert "feat: Chrome Side Panel replaces popup"
    This reverts commit 256742091cc35f9ef1ebedb1a2453c78cf692fa7.

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 // BiliSub background service worker
-chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(() => {});
-
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg.action === 'download') {
     chrome.downloads.download({
